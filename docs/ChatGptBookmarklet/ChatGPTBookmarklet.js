0566a8691fbddd40261a70c8058f81ec03af8c5a
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2023-04-27 22:11:38 +0200

    basis of new bookmarklet

---
+++ docs/ChatGptBookmarklet/ChatGPTBookmarklet.js
@@ -0,0 +1,162 @@
+(function () {
+    var hpsChatGPTBookmarklet = {
+        basePath: '',
+        apikey: '',
+        dialog: null,
+
+        init: async function (basePath, apikey) {
+            this.basePath = basePath;
+            this.apikey = apikey;
+
+            await this.loadCSS();
+            await this.loadHTML();
+
+            document.getElementById('hpsChatGPTCloseTop').addEventListener('click', this.hideDialog.bind(this));
+            document.getElementById('hpsChatGPTCloseBottom').addEventListener('click', this.hideDialog.bind(this));
+            document.getElementById('hpsChatGPTSubmit').addEventListener('click', this.submitQuestion.bind(this));
+            document.getElementById('hpsChatGPTCopyToClipboard').addEventListener('click', this.copyToClipboard.bind(this));
+        },
+
+        loadCSS: function () {
+            return new Promise((resolve) => {
+                var cssLink = document.createElement('link');
+                cssLink.rel = 'stylesheet';
+                cssLink.type = 'text/css';
+                cssLink.href = this.basePath + '/ChatGPTBookmarklet.css';
+                cssLink.onload = resolve;
+                document.head.appendChild(cssLink);
+            });
+        },
+
+        loadHTML: async function () {
+            const response = await fetch(this.basePath + '/ChatGPTBookmarklet.html');
+
+            if (response.ok) {
+                const html = await response.text();
+                this.dialog = document.createElement('div');
+                this.dialog.innerHTML = html;
+                document.body.appendChild(this.dialog);
+            } else {
+                console.error('Failed to load ChatGPTBookmarklet HTML fragment');
+            }
+        },
+
+        showDialog: function () {
+            if (this.dialog) {
+                this.dialog.style.display = 'block';
+            }
+        },
+
+        hideDialog: function () {
+            if (this.dialog) {
+                this.dialog.style.display = 'none';
+            }
+        },
+
+        openDialog: async function () {
+            this.showDialog();
+            const summary = await this.getSummary();
+            document.getElementById('hpsChatGPTAnswer').innerText = summary;
+        },
+
+        submitQuestion: async function () {
+            const question = document.getElementById('hpsChatGPTQuestion').value;
+            const includePageContent = document.getElementById('hpsChatGPTIncludePageContent').checked;
+
+            if (question.trim() !== '') {
+                document.getElementById('hpsChatGPTAnswer').innerText = 'Thinking...';
+                const answer = await this.getAnswer(question, includePageContent);
+                document.getElementById('hpsChatGPTAnswer').innerText = answer;
+            } else {
+                alert('Please enter a question before submitting.');
+            }
+        },
+
+        getPageContent: function () {
+            return document.documentElement.innerText;
+        },
+
+        getSelectedText: function () {
+            const selection = window.getSelection();
+            return selection.toString();
+        },
+
+        copyToClipboard: function () {
+            const answer = document.getElementById('hpsChatGPTAnswer');
+            const selection = window.getSelection();
+            const range = document.createRange();
+            range.selectNodeContents(answer);
+            selection.removeAllRanges();
+            selection.addRange(range);
+
+            try {
+                document.execCommand('copy');
+                alert('Answer copied to clipboard!');
+            } catch (err) {
+                console.error('Unable to copy the answer to clipboard:', err);
+            }
+
+            // Deselect the copied text
+            selection.removeAllRanges();
+        },
+
+        getSummary: async function () {
+            const content = (this.getSelectedText() || this.getPageContent()) + "\n\nTL;DR:";
+            const messages = [{role: 'user', content: content}];
+            const summary = await this.sendChatGPTRequest(messages);
+            return summary;
+        },
+
+        getAnswer: async function (question, includePageContent) {
+            const content = includePageContent ? (this.getSelectedText() || this.getPageContent()) + "\n\nPlease answer the following question with regard to the previous text: " + question : question;
+            const messages = [{role: 'user', content: content}];
+            const answer = await this.sendChatGPTRequest(messages);
+            return answer;
+        },
+
+        sendChatGPTRequest: async function (messages) {
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${this.apikey}`,
+                },
+                body: JSON.stringify({
+                    model: 'gpt-3.5-turbo',
+                    messages: messages,
+                }),
+            };
+
+            let retries = 0;
+            let response;
+
+            while (retries < 3) {
+                response = await fetch('https://api.openai.com/v1/chat/completions', requestOptions);
+
+                if (response.status === 429) {
+                    const retryAfterText = await response.text();
+                    const retryAfterMatch = retryAfterText.match(/Please try again in (\d+)s\./);
+                    if (retryAfterMatch) {
+                        const retryAfter = parseInt(retryAfterMatch[1], 10);
+                        await new Promise(resolve => setTimeout(resolve, (retryAfter + 1) * 1000));
+                        retries++;
+                    } else {
+                        throw new Error('API limit exceeded, but retry time not found.');
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            if (response.ok) {
+                const data = await response.json();
+                return data.choices[0].message.content;
+            } else {
+                throw new Error('Error fetching data from ChatGPT API');
+            }
+        },
+
+    };
+
+    window.hpsChatGPTBookmarklet = hpsChatGPTBookmarklet;
+})();
---
+++ docs/ChatGptBookmarklet/bookmarkletloader.js
@@ -0,0 +1,26 @@
+
+/* Create actual bookmarklet e.g. using https://www.shareprogress.org/bookmarklet/ */
+
+javascript:/*ChatGPTBookmarklet*/(function() {
+  function load(basePath, apikey) {
+    if (!window.hpsChatGPTBookmarklet) {
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = basePath + '/ChatGPTBookmarklet.js';
+      script.onload = function () {
+        window.hpsChatGPTBookmarklet.init(basePath, apikey);
+        window.hpsChatGPTBookmarklet.openDialog();
+      };
+      document.body.appendChild(script);
+    } else {
+      window.hpsChatGPTBookmarklet.openDialog();
+    }
+  }
+  load('http://localhost:63342/chatgpt/docs/ChatGPTBookmarklet', 'XXX');
+})();
+
+// locally from IntelliJ: 'http://localhost:63342/chatgpt/docs/ChatGPTBookmarklet'
+// replace XXX by your API key
+
+// actual bookmarklet to copy into the bookmark, for the deployed version; replace XXX by your API key:
+// TODO
