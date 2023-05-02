692f8df5d208443e3fac1a41133e6b257e1a9b08
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2023-05-02 21:00:41 +0200

    maximize functionality added. Needed ChatGPT-4 to fix missing rule.

--- docs/ChatGptBookmarklet/addFeature.sh
+++ docs/ChatGptBookmarklet/addFeature.sh
@@ -2,11 +2,11 @@
 set -e
 echo adding a new feature to the bookmarklet
 
-mainname=ChatGptBookmarklet
+mainname=ChatGPTBookmarklet
 
-feature="Add a maximize button [+] next to the close button [x] that maximizes the dialog over the whole screen. If the dialog is already maximized, it should be restored to the normal size. When maximized, the button should get the text [-] and when restored, it should get the text [+] again."
+feature="Add a maximize button [+] next to the close button [x] that maximizes the dialog over the whole screen. If the dialog is already maximized, it should be restored to the normal size. When maximized, the button should get the text [-] and when restored, it should get the text [+] again. Set the text of the button in the HTML / with Javascript, not in the CSS. Never use content attribute in CSS."
 
-changehtml="First step: modify $mainname.html file if necessary for the task. The CSS and javascript steps come later - do only output the changed HTML file for now."
+changehtml="First step: modify $mainname.html file if necessary for the task. The CSS and javascript steps come later - do only output the changed HTML file for now. Do not include any CSS or javascript into the HTML file!"
 changecss="Second step: modify the $mainname.css file if necessary for the task. The $mainname.html was already changed for the task and is only included for reference. The javascript modifications come later. The HTML is included for reference, do not include it into your response!"
 changejs="Third step: modify the $mainname.js file. The $mainname.html and $mainname.css files are already changed for the task and are only included for reference. Do not include $mainname.html and $mainname.css into your response!"
 
@@ -21,3 +21,5 @@ fi
 if [ ! -f $mainname.js.orig ]; then
   chatgptfixfile -f $mainname.html -f $mainname.css $mainname.js "$feature" "$changejs"
 fi
+
+echo DONE
