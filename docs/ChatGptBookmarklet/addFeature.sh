708d39210938575461e7f666b81796be7228ac0d
Hans-Peter Stoerr <yu4cheem@techno.ms>
2023-05-02 21:20:46 +0200

    help text

--- docs/ChatGptBookmarklet/addFeature.sh
+++ docs/ChatGptBookmarklet/addFeature.sh
@@ -4,7 +4,8 @@ echo adding a new feature to the bookmarklet
 
 mainname=ChatGPTBookmarklet
 
-feature="Add a maximize button [+] next to the close button [x] that maximizes the dialog over the whole screen. If the dialog is already maximized, it should be restored to the normal size. When maximized, the button should get the text [-] and when restored, it should get the text [+] again. Set the text of the button in the HTML / with Javascript, not in the CSS. Never use content attribute in CSS."
+# feature="Add a maximize button [+] next to the close button [x] that maximizes the dialog over the whole screen. If the dialog is already maximized, it should be restored to the normal size. When maximized, the button should get the text [-] and when restored, it should get the text [+] again. Set the text of the button in the HTML / with Javascript, not in the CSS. Never use content attribute in CSS."
+feature="Add a help button in the bottom button row that sets the answer div to a help text how to use the application."
 
 changehtml="First step: modify $mainname.html file if necessary for the task. The CSS and javascript steps come later - do only output the changed HTML file for now. Do not include any CSS or javascript into the HTML file!"
 changecss="Second step: modify the $mainname.css file if necessary for the task. The $mainname.html was already changed for the task and is only included for reference. The javascript modifications come later. The HTML is included for reference, do not include it into your response!"
