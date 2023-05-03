9d259c219758fe02c26193283a99504bf770dd44
Hans-Peter Stoerr <yu4cheem@techno.ms>
2023-05-03 22:13:28 +0200

    make page to install bookmarklets

--- docs/grabStyles/bookmarkletloader.js
+++ docs/grabStyles/bookmarkletloader.js
@@ -1,7 +1,8 @@
 /** This is the actual bookmarklet that loads the grabStyles javascript library and starts it.
  * I create a separate loader so that I can easily fix things without having to edit the bookmark. */
 
-/* Create actual bookmarklet e.g. using https://www.shareprogress.org/bookmarklet/ */
+/* To create actual bookmarklet use e.g. https://www.shareprogress.org/bookmarklet/, or,
+even better, open https://stoerr.github.io/chatGPTtools/ in the browser and grab it from there. */
 
 javascript:/*net.stoerr.chatGPT.grabStyles*/(function () {
     function load(basePath) {
@@ -22,6 +23,3 @@ javascript:/*net.stoerr.chatGPT.grabStyles*/(function () {
     // load('https://stoerr.github.io/chatGPTtools/grabStyles');
     load('http://localhost:63342/chatgpt/docs/grabStyles');
 })();
-
-// actual bookmarklet to copy into the bookmark comes now. That's an urlencoded version of the above.
-// javascript:(function()%7Bjavascript%3A%2F*net.stoerr.chatGPT.grabStyles*%2F(function () %7B%0A    function load(basePath) %7B%0A        if (!window.netStoerrGrabStyles) %7B%0A            var script %3D document.createElement(%27script%27)%3B%0A            script.type %3D %27text%2Fjavascript%27%3B%0A            script.src %3D basePath %2B %27%2FgrabStyles.js%27%3B%0A            script.onload %3D function () %7B%0A                window.netStoerrGrabStyles.init(basePath)%3B%0A                window.netStoerrGrabStyles.start()%3B%0A            %7D%3B%0A            document.body.appendChild(script)%3B%0A        %7D else %7B%0A            window.netStoerrGrabStyles.start()%3B%0A        %7D%0A    %7D%0A%0A    %2F%2F load(%27https%3A%2F%2Fstoerr.github.io%2FchatGPTtools%2FgrabStyles%27)%3B%0A    load(%27http%3A%2F%2Flocalhost%3A63342%2Fchatgpt%2Fdocs%2FgrabStyles%27)%3B%0A%7D)()%3B%7D)()
