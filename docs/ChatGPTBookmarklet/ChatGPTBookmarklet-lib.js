fb19f0cfbfa1384497f7581f38cd2702a3b5f0f9
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2023-05-31 19:46:35 +0200

    refactor bookmarklet to split files and make files smaller and more manageable

---
+++ docs/ChatGPTBookmarklet/ChatGPTBookmarklet-lib.js
@@ -0,0 +1,188 @@
+(function (window) {
+    window.hpsChatGPTBookmarklet = window.hpsChatGPTBookmarklet || {};
+
+    var hpsChatGPTBookmarklet= {
+        helptext: 'Usage:<br>When you open the dialog, a request to summarize the text of the whole page is automatically sent to ChatGPT. You can also ask your own questions by typing the question into the Ask a question field and submit that to get an answer. If "include page content" is selected, the page text is added. If text was selected before calling this bookmarklet, we take the selected text instead. If the text is more than 2000 words, we replace the middle of the text by ... to avoid hitting ChatGPT limits. You can also copy the answer to the clipboard.',
+
+        init: async function () {
+            if (!this.initialized) {
+                this.initialized = true;
+
+                this.selectedText = this.getSelectedText();
+                this.pageContent = this.getPageContent();
+                this.clipped = false;
+
+                this.answerfield = document.getElementById('hpsChatGPTAnswer');
+                this.getIncludedText(); // sets clipped to true if text was clipped
+                if (this.clipped) {
+                    document.getElementById('clipped').classList.remove('hidden');
+                }
+                // set the focus on the textarea
+                document.getElementById('hpsChatGPTQuestion').focus();
+            }
+        },
+
+        openDialogImpl: async function () {
+            this.init();
+            this.showDialog();
+            try {
+                this.answerfield.innerText = 'Contacting ChatGPT for summary...';
+                const summary = await this.getSummary();
+                this.answerfield.innerText = summary;
+            } catch (e) {
+                this.answerfield.innerText = 'Error: ' + e;
+            }
+        },
+
+        showDialog: function () {
+            if (this.dialog) {
+                this.dialog.style.display = 'block';
+            }
+        },
+
+        hideDialog: function () {
+            if (this.dialog) {
+                this.dialog.style.display = 'none';
+            }
+        },
+
+        handleKeydown: function (event) {
+            if (event.key === 'Escape') {
+                this.hideDialog();
+            }
+        },
+
+        submitQuestion: async function () {
+            const question = document.getElementById('hpsChatGPTQuestion').value;
+            const includePageContent = document.getElementById('hpsChatGPTIncludePageContent').checked;
+
+            if (question.trim() !== '') {
+                this.answerfield.innerText = 'Thinking...';
+                try {
+                    const answer = await this.getAnswer(question, includePageContent);
+                    this.answerfield.innerText = answer;
+                } catch (e) {
+                    this.answerfield.innerText = 'Error: ' + e;
+                }
+            } else {
+                alert('Please enter a question before submitting.');
+            }
+        },
+
+        getPageContent: function () {
+            return document.body.innerText;
+        },
+
+        getSelectedText: function () {
+            const selection = window.getSelection();
+            return selection.toString();
+        },
+
+        getIncludedText: function () {
+            let thetext = this.selectedText || this.pageContent;
+            // if thetext contains more than 2000 words, we remove the middle so that it's now 2000 words
+            // this is to avoid hitting the API limit of 2048 tokens
+            const whitespaceregex = /\s+/gm;
+            if (thetext.split(whitespaceregex).length > 1800) {
+                const words = thetext.trim().split(whitespaceregex);
+                thetext = words.slice(0, 900).join(" ") + "\n...\n" + words.slice(words.length - 900).join(" ");
+                this.clipped = true;
+                if (document.getElementById('clipped')) {
+                    document.getElementById('clipped').classList.remove('hidden');
+                }
+            }
+            return thetext;
+        },
+
+        copyToClipboard: function () {
+            const answer = this.answerfield;
+            const selection = window.getSelection();
+            const range = document.createRange();
+            range.selectNodeContents(answer);
+            selection.removeAllRanges();
+            selection.addRange(range);
+
+            try {
+                document.execCommand('copy');
+                alert('Answer copied to clipboard!');
+            } catch (err) {
+                console.error('Unable to copy the answer to clipboard:', err);
+            }
+
+            // Deselect the copied text
+            selection.removeAllRanges();
+        },
+
+        getSummary: async function () {
+            const content = "```\n" + this.getIncludedText() + "\n``\n" +
+                "=== Instructions ===\n" +
+                "Please determine what language the text is written in, do not output that languabe but use it to create a summary of this text.";
+            const messages = [{role: 'user', content: content}];
+            const summary = await this.sendChatGPTRequest(messages);
+            return summary;
+        },
+
+        getAnswer: async function (question, includePageContent) {
+            const content = includePageContent ? this.getIncludedText() + "\n\nPlease answer the following question with regard to the previous text using the language the question is asked:\n" + question : question;
+            const messages = [{role: 'user', content: content}];
+            const answer = await this.sendChatGPTRequest(messages);
+            return answer;
+        },
+
+        sendChatGPTRequest: async function (messages) {
+            const requestOptions = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${this.apikey}`,
+                },
+                body: JSON.stringify({
+                    model: 'gpt-3.5-turbo',
+                    messages: messages,
+                }),
+            };
+
+            let retries = 0;
+            let response;
+
+            while (retries < 3) {
+                response = await fetch('https://api.openai.com/v1/chat/completions', requestOptions);
+
+                if (response.status === 429) {
+                    const retryAfterText = await response.text();
+                    const retryAfterMatch = retryAfterText.match(/Please try again in (\d+)s\./);
+                    if (retryAfterMatch) {
+                        const retryAfter = parseInt(retryAfterMatch[1], 10);
+                        await new Promise(resolve => setTimeout(resolve, (retryAfter + 1) * 1000));
+                        retries++;
+                    } else {
+                        throw new Error('API limit exceeded, but retry time not found.');
+                    }
+                } else {
+                    break;
+                }
+            }
+
+            if (response.ok) {
+                const data = await response.json();
+                return data.choices[0].message.content;
+            } else {
+                throw new Error('Problem fetching data from ChatGPT API: ' + response.status + ' ' + response.statusText + ' ' + await response.text());
+            }
+        },
+
+        toggleMaximize: function () {
+            const dialogContainer = document.querySelector('.hpsChatGPTDialog-container');
+            const maximizeButton = document.getElementById('hpsChatGPTMaximize');
+            const isMaximized = dialogContainer.classList.toggle('hps-chatgpt-maximize-maximized');
+            maximizeButton.innerText = isMaximized ? '[-]' : '[+]';
+        },
+
+        showHelp: function () {
+            this.answerfield.innerHTML = hpsChatGPTBookmarklet.helptext;
+        }
+
+    };
+
+    Object.assign(window.hpsChatGPTBookmarklet, hpsChatGPTBookmarklet);
+})(window);
---
+++ docs/OpenAIChatDiagramHelper/grabStyles.js
@@ -0,0 +1,319 @@
+(function () {
+    var netStoerrGrabStyles = {
+        basePath: '',
+
+        init: function (basePath) {
+            this.basePath = basePath;
+        },
+
+        generalHelp: function () {
+            return `
+            Highlight an element with the mouse and then click on it to get the HTML and CSS rules for it copied to the clipboard and logged to the console.
+            `;
+        },
+
+        start: function () {
+            try {
+                const that = this;
+                this.selectElement(async function (element) {
+                    var css = that.collectCSSRules(element);
+                    var elementdescr = "Selected element: " + element.tagName.toLowerCase() + "#" + element.id + "." + element.className;
+                    var result = elementdescr;
+                    result = result + "\n\n" + that.relevantHTML(element, css) + "\n\n";
+                    console.log(result);
+                    const wordcount = result.split(/\s+/).length;
+                    that.copyToClipboard(result, function () {
+                        alert("Rules for the element copied to clipboard and logged: " + elementdescr + "\n\n" + wordcount + " words.");
+                    });
+
+                });
+            } catch (error) {
+                console.error(error);
+                alert(error);
+            }
+        },
+
+        relevantHTML: function (element, css) {
+            var html = element.outerHTML.trim() + "\n";
+            let ancestor = element.parentElement;
+            while (ancestor.tagName.toUpperCase() !== 'HTML') {
+                html = `<${ancestor.tagName.toLowerCase()}${(ancestor.id ? ` id="${ancestor.id}"` : '')}${(ancestor.className ? ` class="${ancestor.className}"` : '')}>\n${html}</${ancestor.tagName.toLowerCase()}>`;
+                ancestor = ancestor.parentElement;
+            }
+            html = `<${ancestor.tagName.toLowerCase()}${(ancestor.id ? ` id="${ancestor.id}"` : '')}${(ancestor.className ? ` class="${ancestor.className}"` : '')}>\n<head><style>\n${css}\n</style></head>${html}</${ancestor.tagName.toLowerCase()}>`;
+            return html.replace(/<!--(.*?)-->/sg, '').replace(/\n(\s*\n)+/gm, '\n').replace(/\s+$/, ' ');
+        },
+
+        copyToClipboard: function (text, callback) {
+            if (!navigator.clipboard) {
+                const tempEl = document.createElement("textarea");
+                tempEl.value = text;
+                document.body.appendChild(tempEl);
+                tempEl.select();
+                document.execCommand("copy");
+                document.body.removeChild(tempEl);
+                callback();
+            } else {
+                navigator.clipboard.writeText(text).then(
+                    function () {
+                        callback();
+                    })
+                    .catch(
+                        function (error) {
+                            alert("Could not copy to clipboard: " + error);
+                            callback();
+                        });
+            }
+        },
+
+        collectCSSRules: function (element) {
+            function collectAncestorsAndDescendants(element) {
+                var ancestors = [];
+                var ancestor = element.parentElement;
+                while (ancestor) {
+                    ancestors.push(ancestor);
+                    ancestor = ancestor.parentElement;
+                }
+
+                var descendants = [];
+                var nodes = [element];
+                while (nodes.length > 0) {
+                    var node = nodes.shift();
+                    descendants.push(node);
+                    var children = node.children;
+                    for (const child of children) {
+                        nodes.push(child);
+                    }
+                }
+
+                return ancestors.concat(descendants);
+            }
+
+            function anyElementMatches(elements, selector) {
+                if (!selector) return false;
+                for (const element of elements) {
+                    try {
+                        if (element.matches(selector)) {
+                            return true;
+                        }
+                        // check whether selector with all occurrences of :before or :after replaced by the empty string matches
+                        const selectorWithoutBeforeAfter = selector.replace(/::?(before|after)/g, '');
+                        if (element.matches(selectorWithoutBeforeAfter)) {
+                            return true;
+                        }
+                    } catch (error) {
+                        // that might happen if the selector is empty , can't help but ignore this
+                    }
+                }
+                return false;
+            }
+
+            var elements = collectAncestorsAndDescendants(element);
+
+            var allCSS = "";
+            const sheets = document.styleSheets;
+            for (const sheet of sheets) {
+                try {
+                    const rules = sheet.rules || sheet.cssRules;
+                    let sheetCSS = "";
+                    for (const rule of rules) {
+                        if (rule.selectorText) {
+                            if (anyElementMatches(elements, rule.selectorText)) {
+                                sheetCSS += rule.cssText + "\n";
+                            }
+                        } else { // what would that be?
+                            console.error("Rule without selectorText ", rule, " : ", JSON.stringify(rule));
+                        }
+                    }
+                    if (sheetCSS) {
+                        allCSS += "\n\n/* STYLESHEET: " + sheet.href + " */\n";
+                        allCSS += sheetCSS;
+                    }
+                } catch (error) {
+                    console.error("Error while processing stylesheet ", sheet, " : ", error);
+                }
+            }
+            return allCSS;
+        },
+
+        selectElement: function (callback) {
+            let selectedElement = null;
+            let highlightedElement = null;
+            let that = this;
+
+            function handleMouseMove(event) {
+                const elementUnderMouse = findElementUnderMouse(event.clientX, event.clientY);
+                if (highlightedElement !== elementUnderMouse) {
+                    if (highlightedElement !== null) {
+                        resetElement(highlightedElement);
+                    }
+                    if (elementUnderMouse !== null) {
+                        highlightedElement = elementUnderMouse;
+                        highlightElement(highlightedElement);
+                    } else {
+                        highlightedElement = null;
+                    }
+                }
+            }
+
+            function handleKeyPress(event) {
+                if (event.key === 'Escape') {
+                    removeListeners();
+                    if (highlightedElement !== null) {
+                        resetElement(highlightedElement);
+                    }
+                } else if (event.key === 'ArrowUp' || event.key === 'p') {
+                    event.preventDefault();
+                    if (highlightedElement && highlightedElement.parentElement) {
+                        resetElement(highlightedElement);
+                        highlightedElement = highlightedElement.parentElement;
+                        highlightElement(highlightedElement);
+                    }
+                } else if (event.key === 'Enter') {
+                    event.preventDefault();
+                    if (highlightedElement !== null) {
+                        resetElement(highlightedElement);
+                        selectedElement = highlightedElement;
+                        highlightedElement = null;
+                        removeListeners();
+                        callback(selectedElement);
+                    }
+                } else if (event.key === 'd') {
+                    event.preventDefault();
+                    if (highlightedElement !== null) {
+                        const parent = highlightedElement.parentElement;
+                        resetElement(highlightedElement);
+                        highlightedElement.remove();
+                        highlightedElement = parent;
+                        highlightElement(highlightedElement);
+                    }
+                } else if (event.key === 'D') {
+                    event.preventDefault();
+                    if (highlightedElement !== null) {
+                        deleteAllExceptSelectedAndAncestors(highlightedElement);
+                    }
+                } else if (event.key === '?' || event.key === 'h') {
+                    event.preventDefault();
+                    showHelp();
+                }
+            }
+
+            function showHelp() {
+                const helpText = `
+                - Mouse hover: Highlight an element
+                - Left-click: Select the highlighted element and perform the action
+                - Control-click: Delete the highlighted element
+                - Alt-click: Delete everything except the highlighted element and its ancestors and descendants
+                - Escape: Stop the selection process
+                - ArrowUp: Highlight the parent of the highlighted element
+                - Enter: Select the highlighted element
+                - d: Delete the highlighted element
+                - D: Delete everything except the highlighted element and its ancestors and descendants
+                - ? or h: Show this help message
+                `;
+                alert(that.generalHelp() + helpText);
+            }
+
+            function handleMouseClick(event) {
+                if (highlightedElement !== null && !event.altKey && !event.metaKey) {
+                    resetElement(highlightedElement);
+                    selectedElement = highlightedElement;
+                    highlightedElement = null;
+                    removeListeners();
+                    callback(selectedElement);
+                }
+            }
+
+            function handleMetaClick(event) {
+                if (highlightedElement !== null && !event.altKey && event.metaKey) {
+                    event.preventDefault();
+                    highlightedElement.remove();
+                }
+            }
+
+            function handleAltClick(event) {
+                if (highlightedElement !== null && event.altKey && !event.metaKey) {
+                    event.preventDefault();
+                    deleteAllExceptSelectedAndAncestors(highlightedElement);
+                }
+            }
+
+            document.addEventListener("mousemove", handleMouseMove);
+            document.addEventListener("click", handleMouseClick);
+            document.addEventListener("keydown", handleKeyPress);
+            document.addEventListener("click", handleMetaClick);
+            document.addEventListener("click", handleAltClick);
+
+            function removeListeners() {
+                document.removeEventListener("mousemove", handleMouseMove);
+                document.removeEventListener("click", handleMouseClick);
+                document.removeEventListener("keydown", handleKeyPress);
+                document.removeEventListener("click", handleMetaClick);
+                document.removeEventListener("click", handleAltClick);
+            }
+
+            function findElementUnderMouse(x, y) {
+                const element = document.elementFromPoint(x, y);
+                return element !== null ? element : document.body;
+            }
+
+            function highlightElement(element) {
+                const oldstyle = element.getAttribute("style");
+                const oldtitle = element.getAttribute("title");
+                if (oldstyle) {
+                    element.dataset.netStoerrGrabStylesOriginalStyle = oldstyle;
+                    element.dataset.netStoerrGrabStylesOriginalTitle = oldtitle;
+                }
+                element.style.setProperty('outline', "3px solid red", "important");
+                element.setAttribute("title", "\n    Press h for help.    \n");
+            }
+
+            function resetElement(element) {
+                const oldstyle = element.dataset.netStoerrGrabStylesOriginalStyle;
+                const oldtitle = element.dataset.netStoerrGrabStylesOriginalTitle;
+                if (oldstyle) {
+                    element.setAttribute("style", oldstyle);
+                } else {
+                    element.removeAttribute("style");
+                }
+                if (oldtitle) {
+                    element.setAttribute("title", oldtitle);
+                } else {
+                    element.removeAttribute("title");
+                }
+                delete element.dataset.netStoerrGrabStylesOriginalStyle;
+            }
+
+            function getOriginalBorderStyle(element) {
+                return element.dataset.originalBorderStyle || "none";
+            }
+
+            function deleteAllExceptSelectedAndAncestors(element) {
+                const ancestors = [];
+                let ancestor = element.parentElement;
+                while (ancestor && ancestor.tagName.toUpperCase() !== 'HTML') {
+                    ancestors.push(ancestor);
+                    ancestor = ancestor.parentElement;
+                }
+
+                const body = document.body;
+                while (body.firstChild) {
+                    body.firstChild.remove();
+                }
+
+                let parent = body;
+                for (const ancestor of ancestors) {
+                    const clonedAncestor = ancestor.cloneNode(false);
+                    parent.appendChild(clonedAncestor);
+                    parent = clonedAncestor;
+                }
+
+                parent.appendChild(element);
+            }
+
+        },
+
+    };
+
+    window.netStoerrGrabStyles = netStoerrGrabStyles;
+})();
