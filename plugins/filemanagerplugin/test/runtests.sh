4ea0207456d75dfb45fa282ba0331738ef3cd0b2
Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2023-06-11 15:41:46 +0200

    continue test of FileManagerPlugin

--- plugins/filemanagerplugin/test/runtests.sh
+++ plugins/filemanagerplugin/test/runtests.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-set -vx
+overwrite=false
+# if the first argument is -o, overwrite the expected files: set overwrite to true
+if [ "$1" == "-o" ]; then
+    overwrite=true
+fi
+
 # random port for testing the app at
 port=7364
 # cd to the directory of the script
@@ -23,17 +28,26 @@ function executetest() {
     expected="$3"
     actual=".tmp/$(basename $expected)"
     echo "testing $url"
-    curl $args $url -o $actual
+    curl -s $args $url -o $actual
+    echo >> $actual
+    # if expected does not exist, copy actual to expected and add failure
     if ! diff -u $expected $actual; then
-        failures="$failures\nGET $url"
+        failures="$failures $expected"
+    fi
+    # if expected does not exist or overwrite is true, overwrite expected with actual
+    if [ ! -f $expected ] || [ "$overwrite" == "true" ]; then
+        cp $actual $expected
+        echo "CREATED: $expected"
+        failures="$failures $expected"
     fi
 }
 
 executetest /.well-known/ai-plugin.json "" ../ai-plugin.json
 executetest /dirreaderplugin.yaml "" ../dirreaderplugin.yaml
+executetest /listFiles?path=. "" listFiles.json
 
 # if there are failures, print them out and exit with a non-zero exit code
 if [ -n "$failures" ]; then
-    echo -e "FAILURES:\n$failures"
+    echo -e "FAILURES: $failures"
     exit 1
 fi
