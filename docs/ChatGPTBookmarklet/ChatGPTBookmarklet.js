23004af2c68de19b524c33c65e192f62536cb638
Dr. Hans-Peter Stoerr <hans-peter+git-noreply@stoerr.net>
2024-02-20 17:46:03 +0000

    try to prevent overlapping of other stuff over ChatGPT dialog

--- docs/ChatGPTBookmarklet/ChatGPTBookmarklet.js
+++ docs/ChatGPTBookmarklet/ChatGPTBookmarklet.js
@@ -41,6 +41,8 @@
                         that.dialog.appendChild(element);
                     });
                     document.body.appendChild(that.dialog);
+
+                    that.sanitizeZIndexes(root);
                 } else {
                     console.error('Failed to load ChatGPTBookmarklet HTML fragment because of ' + response.status + ' ' + response.statusText);
                 }
@@ -50,6 +52,20 @@
         } catch (e) {
             console.error('Failed to load ChatGPTBookmarklet HTML fragment because of ' + e);
         }
+    };
+
+    /** We find elements with z-index more than 999999999 that would be above us and reset their z-index below 999999999. */
+    that.sanitizeZIndexes = function (root) {
+        root.querySelectorAll('*').forEach((element) => {
+            const zIndex = parseInt(window.getComputedStyle(element).zIndex);
+            if (zIndex > 999999999) {
+                var newZindex = zIndex / 3; // still smaller than 999999999 and might keep relative z-indexes
+                if (zIndex > 2147383647) { // keep relative z-indexes if they are extremely high.
+                    newZindex = zIndex - 1147483649;
+                }
+                element.style.setProperty('z-index', newZindex.toString(), 'important');
+            }
+        });
     };
 
     that.openDialog = async function () {
